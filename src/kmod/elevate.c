1f02a00aa26f143ff016663116d02a544bebd6f8
Jasper Lowell <jtalowell@protonmail.com>
2019-07-21 20:00:32 +1000

    Use shell scripts instead of compiled binaries

--- src/kmod/elevate.c
+++
@@ -1,6 +0,0 @@
- #include <stdlib.h>
-
-int main(int argc, char**argv){
-	system("mkdir escalate");
-	return 0;
-}
--- src/kmod/install.c
+++
@@ -1,6 +0,0 @@
- #include <stdlib.h>
-
-int main(int argc, char**argv){
-	system("make load");
-	return 0;
-}
